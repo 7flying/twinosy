7ba8965da7e01a6b959e27ba813fbf12149377ff
7flying <diez.f.irene@gmail.com>
2015-03-14 17:05:39 +0100

    Save tests

--- twinosy/test.py
+++ twinosy/test.py
@@ -1,27 +1,93 @@
+import time
+from random import randint
 import networkx as nx
 import matplotlib.pyplot as plt
 
-graph = nx.Graph()
+from graph_tool.all import *
 
-graph.add_node("user")
-graph.add_node("user2")
+def test1():
+    graph = nx.Graph()
 
-graph.add_edge("user", "user2")
+    graph.add_node("user")
+    graph.add_node("user2")
 
-print "Number of nodes:", graph.number_of_nodes()
-print "Number of edges:", graph.number_of_edges()
+    graph.add_edge("user", "user2")
+
+    print "Number of nodes:", graph.number_of_nodes()
+    print "Number of edges:", graph.number_of_edges()
 
 
-dir_graph = nx.DiGraph()
-dir_graph.add_node("user", account="@user")
-dir_graph.add_node("user-2", account="@user2")
-dir_graph.add_edge("user", "user-2")
-#dir_graph.add_edge("user-2", "user")
+    dir_graph = nx.DiGraph()
+    dir_graph.add_node("user", account="@user")
+    dir_graph.add_node("user-2", account="@user2")
+    dir_graph.add_edge("user", "user-2")
+    #dir(_graph.add_edge("user-2", "user")
 
-pos = nx.graphviz_layout(dir_graph)
-nx.draw_networkx_nodes(dir_graph,pos,node_size=300,node_color='#9933FF',alpha=0.4)
-nx.draw_networkx_edges(dir_graph,pos,edge_color='#3399FF',node_size=0,
+    pos = nx.graphviz_layout(dir_graph)
+    nx.draw_networkx_nodes(dir_graph,pos,node_size=300,node_color='#9933FF',alpha=0.4)
+    nx.draw_networkx_edges(dir_graph,pos,edge_color='#3399FF',node_size=0,
                        width=1,alpha=0.4)
-nx.draw_networkx_labels(dir_graph,pos,fontsize=14)
-plt.axis('off')
-plt.show()
+    nx.draw_networkx_labels(dir_graph,pos,fontsize=14)
+    #nx.draw_graphviz(dir_graph)
+    
+    plt.axis('off')
+    plt.show()
+
+def test2():
+    time_one = time.time()
+    dir_graph = nx.DiGraph()
+    total = 2000
+    for i in range(total):
+        account = "USER-" + str(i)
+        dir_graph.add_node(str(i), account=account)
+        if i != 0:
+            if i == total - 1:
+                dir_graph.add_edge(str(i), str(0))
+            dir_graph.add_edge(str(i), str(i - 1))
+            dir_graph.add_edge(str(i), str(randint(0, i)))
+            
+    time_two = time.time()
+    print "Time took to add %d nodes & edges: %s" % (total, str((time_two - time_one)))
+    pos = nx.graphviz_layout(dir_graph)
+    nx.draw_networkx_nodes(dir_graph,pos,node_size=300,node_color='#9933FF',alpha=0.4)
+    nx.draw_networkx_edges(dir_graph,pos,edge_color='#3399FF',node_size=0,
+                       width=1,alpha=0.4)
+    nx.draw_networkx_labels(dir_graph,pos,fontsize=14)
+    plt.axis('off')
+    time_three = time.time()
+    print "Time took to paint the graph: %s" % (str(time_three - time_one))
+    time_four = time.time()
+    #nx.draw(dir_graph)
+    nx.write_dot(dir_graph, "test2.dot") # then dot file.dot -T png > output.png
+    print "Time took to generate dot file: %s" % (str(time_four - time_one))
+    print "Total: %s" % (str(time_four - time_one))
+    #plt.show()
+    #plt.savefig("test2.png")
+   
+
+def test3():
+    g = Graph()
+    g.add_vertex(2) # Adds N vertices
+    label = g.new_vertex_property("string")
+    label[g.vertex(0)] = "some_0"
+    label[g.vertex(1)] = "some_1" 
+    g.add_edge(1, 0)
+    graph_draw(g, vertex_text=label,output="test3.png")
+
+def test4():
+    g = Graph()
+    g.add_vertex(2000) # Adds N vertices
+    label = g.new_vertex_property("string")
+    for i in range(2000):
+        label[g.vertex(i)] = "USER" + str(i)
+        if i != 0:
+            g.add_edge(i, i-1)
+    graph_draw(g, vertex_text=label, output_size=(2000,2000),output="test4.png")
+
+
+if __name__ == '__main__':
+    #test1()
+    test2()
+    #test3()
+    #test4()
+    #test5()
