901cda949f692fc3f16e11ac9d5faad0b1887ea0
7flying <diez.f.irene@gmail.com>
2015-07-31 14:48:45 +0200

    Changed NtoN relationships on domain, update requirements
    -NtoN relationships within users were wrong, fix it.
    -Add tweepy and SQLAlchemy to requirements.txt

--- twinosy/domain.py
+++ twinosy/domain.py
@@ -1,9 +1,29 @@
 # -*- coding: utf-8 -*-
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Boolean, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy import Column, Boolean, Integer, String, Table, \
+     ForeignKey
+from sqlalchemy.orm import relationship, backref, sessionmaker
+# testing
+from sqlalchemy import create_engine
+
+
+engine = create_engine('sqlite:///:memory:', echo=False)
 
 Base = declarative_base()
+following = Table('following', Base.metadata,
+                  Column('user', Integer,
+                         ForeignKey('user.uid', ondelete='CASCADE'),
+                         primary_key=True),
+                  Column('follows', Integer,
+                         ForeignKey('user.uid', ondelete='CASCADE'),
+                         primary_key=True))
+followers = Table('followers', Base.metadata,
+                  Column('user', Integer,
+                         ForeignKey('user.uid', ondelete='CASCADE'),
+                         primary_key=True),
+                  Column('follower', Integer,
+                         ForeignKey('user.uid', ondelete='CASCADE'),
+                         primary_key=True))
 
 class User(Base):
     """A Twitter user"""
@@ -13,21 +33,34 @@ class User(Base):
     account = Column(String, unique=True, nullable=False)
     description = Column(String)
     official = Column(Boolean)
-    following = relationship("Following", backref="user",
-                             order_by="Following.follows")
-    followers = relationship("Followers", backref="user",
-                             order_by="Followers.follower")
+    following = relationship("User", secondary=following,
+                             primaryjoin=uid==following.c.user,
+                             secondaryjoin=uid==following.c.follows)
+    followers = relationship("User", secondary=followers,
+                             primaryjoin=uid==followers.c.user,
+                             secondaryjoin=uid==followers.c.follower)
 
-class Following(Base):
-    """Following table"""
-    __tablename__ = 'following'
+    def __repr__(self):
+        return ("<User(uid='%d', account='%s', description='%s'," + \
+                " official='%s')>") % (self.uid, self.account,
+                                       self.description, self.official)
 
-    user = Column(ForeignKey('user.uid', ondelete='CASCADE'))
-    follows = Column(ForeignKey('user.uid', ondelete='CASCADE'))
-
-class Followers(Base):
-    """Followers table """
-    __tablename__ = 'followers'
-
-    user = Column(ForeignKey('user.uid', ondelete='CASCADE'))
-    follower = Column(ForeignKey('user.uid', ondelete='CASCADE'))
+if __name__ == '__main__':
+    print " ~ Starting tests ~ \n"
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    # Without a engine:
+    # Session = sessionmaker()
+    # Session.configure(bind=engine)
+    session = Session()
+    tuser = User(account='7flying', description='SUPER', official=False)
+    session.add(tuser)
+    tuser2 = User(account='another', description='AND', official=False,
+                  following=[tuser])
+    session.add(tuser2)
+    session.commit()
+    print tuser
+    print tuser2
+    print tuser2.following
+    session.close()
+    
